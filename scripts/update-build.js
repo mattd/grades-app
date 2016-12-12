5806246a8e85d386198d804e398ce4f2139d673a
Matt Dawson <matthewtdawson@gmail.com>
2016-12-12 12:51:29 -0500

    Fix build timestamps

--- scripts/update-build.js
+++ scripts/update-build.js
@@ -3,7 +3,9 @@ const admin = require('firebase-admin');
 const target = process.env['DEPLOY_TARGET'];
 const key = require('../config/firebase/service-keys/' + target + '.json');
 const config = require('../config/firebase/' + target);
+
 const commit = process.argv[2].slice(0, 7);
+const timestamp = process.env['BUILD_TIMESTAMP'];
 const version = require('../package').version;
 
 const app = admin.initializeApp({
@@ -16,7 +18,7 @@ const app = admin.initializeApp({
 
 admin.database().ref('/build').set({
     commit: commit,
-    timestamp: Date.now(),
+    timestamp: timestamp,
     version: version
 }).then(() => {
     app.delete();
